3e46e9abec9a0209b5bddb4d49137df49322381d
Alessandro <panzieri.ale@gmail.com>
2024-02-05 10:08:37 +0100

    add astro vercel

--- src/frontend/astro.config.mjs
+++ src/frontend/astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
-export default defineConfig({});
+export default defineConfig({
+    output: "server",
+    adapter: vercel()
+});
