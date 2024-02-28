cc3d5f060f78d0d059eca1ff9b71ca47709b9ead
Alessandro <panzieri.ale@gmail.com>
2024-02-28 10:02:25 +0100

    edit main.py and improve dockerfiles

--- src/backend/main.py
+++ src/backend/main.py
@@ -15,7 +15,3 @@ app.add_middleware(
 app.include_router(administratives.router, prefix = "/administratives", tags = ["administratives"])
 app.include_router(employees.router, prefix = "/employees", tags = ["employees"])
 app.include_router(events.router, prefix = "/events", tags = ["events"])
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", reload = True)
