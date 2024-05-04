7c4c8225aeb564fdc89e053418fa03bc7725e023
alessandro <panzieri.ale@gmail.com>
2024-05-04 23:01:50 +0200

    from keycloak legacy to latest keycloak, upgrade package.json and other small changes

--- src/frontend/src/lib/keycloak.js
+++ src/frontend/src/lib/keycloak.js
@@ -3,7 +3,7 @@ import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
     realm: "GSS",
     clientId: "GSS",
-    url: "http://localhost:8080/auth"
+    url: "http://keycloak:8080/"
 });
 
 export default keycloak;
