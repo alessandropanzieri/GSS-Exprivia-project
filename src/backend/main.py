8e3b0d9950ee7a00882ce06f1d99e168eaf76c72
alessandro <panzieri.ale@gmail.com>
2024-05-05 05:19:59 +0200

    backend keycloak implementation

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,17 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi_keycloak import FastAPIKeycloak
 from fastapi.middleware.cors import CORSMiddleware
-from routes import administrators, employees, events
+from routes import events, employees, administrators
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_headers = ["*"],
     allow_origins = ["*"],
     allow_credentials = True,
-    allow_methods = ["GET", "POST", "PUT", "DELETE"]
+    allow_methods = ["POST", "GET", "PUT", "DELETE"]
 )
 
-app.include_router(administrators.router, prefix = "/administrators", tags = ["administrators"])
-app.include_router(employees.router, prefix = "/employees", tags = ["employees"])
-app.include_router(events.router, prefix = "/events", tags = ["events"])
+idp = FastAPIKeycloak(
+    realm = "GSS",
+    client_id = "GSS",
+    server_url = "http://keycloak:8080/",
+    callback_uri = "http://backend:8000/docs",
+    client_secret = "5RPPSMwANsWE0fXTIyC5qXIfNKSu6P7f",
+    admin_client_secret = "k53cbpQE7B2qRL1j9qoOSexHLHnOB6xE"
+)
+idp.add_swagger_config(app)
+
+app.include_router(events.router, prefix = "/events", tags = ["events"], dependencies = [Depends(idp.get_current_user())])
+app.include_router(employees.router, prefix = "/employees", tags = ["employees"], dependencies = [Depends(idp.get_current_user())])
+app.include_router(administrators.router, prefix = "/administrators", tags = ["administrators"], dependencies = [Depends(idp.get_current_user())])
